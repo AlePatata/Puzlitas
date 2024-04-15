03f6b51d9e6410a21fc3dadc7c2a0974be745792
AlePatata <alejandra.campos.u@ug.uchile.cl>
2024-04-14 22:27:21 -0400

    a

--- Proyecto Godot/Light.gd
+++ Proyecto Godot/Light.gd
@@ -4,6 +4,6 @@ extends CharacterBody2D
 
 func _physics_process(delta) -> void:
 	global_position = get_global_mouse_position()
-	while not is_on_wall(): 
-		background.global_position = get_global_mouse_position()
 	
+	#while not is_on_wall(): 
+	#	background.global_position = get_global_mouse_position()
